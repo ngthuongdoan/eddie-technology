2468a58e03d9a4df04559e324584adcc9f078abd
ngthuongdoan <ngthuongdoan@gmail.com>
2021-09-16 15:25:19 +0700

    fix: :bug: tailwind unused class

--- src/components/Common/TheNavigation.tsx
+++ src/components/Common/TheNavigation.tsx
@@ -11,11 +11,23 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 const TheNavigation: FunctionComponent<Props> = (props: Props): JSX.Element => {
+  const justifyItem = () => {
+    switch (props.position) {
+      case 'start':
+        return 'justify-start';
+      case 'center':
+        return 'justify-center';
+      case 'end':
+        return 'justify-end';
+    }
+    return '';
+  };
+
   return (
     <>
-      <ul className={`list-none w-full inline-flex items-center gap-5 justify-${props.position} ${props.className}`}>
+      <ul className={`list-none w-full flex items-center gap-5 ${justifyItem()} ${props.className ? props.className : ''}`}>
         {props.items.map((item, index) => (
-          <li key={index} className={`flex ${props.direction === 'vertical' && 'flex-col'} items-center justify-center gap-2 text-${props.size}`}>
+          <li key={index} className={`flex ${props.direction === 'vertical' ? 'flex-col' : ''} items-center justify-center gap-2 text-${props.size}`}>
             {(item as Feature).icon && <FontAwesomeIcon icon={(item as Feature).icon} size={props.size} />}
             <span>{(item as Feature).title}</span>
           </li>
