872190aa734f30112590e257ed1f4046e74dacbd
ngthuongdoan <ngthuongdoan@gmail.com>
2021-09-20 12:01:55 +0700

    feat: :sparkles: firebase storage

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -5,11 +5,13 @@ import { Route, Switch } from 'react-router-dom';
 // TODO: Import pages to render
 import ErrorLayout from '../layout/ErrorLayout';
 import DefaultLayout from '../layout/DefaultLayout';
+import UploadLayout from '../layout/UploadLayout';
 
 const AppRouter: React.FC = () => {
   return (
     <Switch>
       <Route path="/404" component={ErrorLayout} />
+      <Route path="/upload" component={UploadLayout} />
       <Route path="/" component={DefaultLayout} />
     </Switch>
   );
