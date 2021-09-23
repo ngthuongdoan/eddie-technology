2eaa6b75b7d3c82a2f715c6cd7ba68a16dca6d24
ngthuongdoan <ngthuongdoan@gmail.com>
2021-09-23 11:14:24 +0700

    fix: :heavy_minus_sign: remove react snap

---
+++ src/@types/react-progress-2.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-progress-2' {
+  const Progress: React.FC = (): JSX.Element => {};
+  export default Meta;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './assets/scss/index.scss';
 import './plugins';
-import { hydrate, render } from 'react-dom';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-if (rootElement!.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+// Single-page application (SPA)
+ReactDOM.render(<App />, document.getElementById('root'));
