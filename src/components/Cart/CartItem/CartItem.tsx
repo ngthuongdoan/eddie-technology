b58c9d440ba0defd1b85d5e1e4290ba0e8ec0361
ngthuongdoan <ngthuongdoan@gmail.com>
2021-09-30 09:30:36 +0700

    fix: :bug: price listedprice wrong display

--- src/components/Cart/CartItem/CartItem.tsx
+++ src/components/Cart/CartItem/CartItem.tsx
@@ -4,6 +4,7 @@ import Card from '@components/UI/Card/Card';
 import React from 'react';
 import { useAppDispatch } from '@hooks/use-app-dispatch';
 import { cartActions } from '@store/modules/cart/reducer';
+import { Link } from 'react-router-dom';
 
 interface Props {
   item: CartProduct;
@@ -20,11 +21,13 @@ const CartItem: React.FC<Props> = ({ item }): JSX.Element => {
   };
   return (
     <Card className="flex items-center justify-start py-2 px-3">
-      <div className="bg-center bg-contain bg-no-repeat rounded-md w-32 h-32" style={{ backgroundImage: `url(${item.images[0]})` }}></div>
-      <div className="w-full ml-10 flex-grow">
-        <h1 className="font-bold text-black text-sm mb-2">{item.name}</h1>
-        <Price className="font-thin text-xs" product={item}></Price>
-      </div>
+      <Link className="flex items-center flex-grow w-full" to={`/product/${item.id}`}>
+        <div className="bg-center bg-contain bg-no-repeat rounded-md w-32 h-32" style={{ backgroundImage: `url(${item.images[0]})` }}></div>
+        <div className="w-full ml-10 ">
+          <h1 className="font-bold text-black text-sm mb-2">{item.name}</h1>
+          <Price className="font-thin text-xs" product={item}></Price>
+        </div>
+      </Link>
       <div className="flex items-center font-bold text-lg mr-10 gap-3">
         <button className="bg-primary text-white font-bold p-1 rounded-full" type="button" onClick={() => removeItem()} aria-hidden="true">
           −
