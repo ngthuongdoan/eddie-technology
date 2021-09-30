77f23906edc0380c9160ab1f85e58aef74811ac2
ngthuongdoan <ngthuongdoan@gmail.com>
2021-09-30 10:36:30 +0700

    feat: :lipstick: add cart summary section

--- src/pages/cart/CartPage.tsx
+++ src/pages/cart/CartPage.tsx
@@ -1,18 +1,27 @@
+import NoValue from '@components/Common/NoValue/NoValue';
+import { CartProduct } from '@model/Product';
+import { RootState } from '@model/ReduxType';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import CheckoutInformation from './CheckoutInformation';
 import ItemsList from './ItemsList';
 
 interface Props {}
 
 const CartPage: React.FC<Props> = (props): JSX.Element => {
+  const products = useSelector<RootState>((state) => state.cart.products) as CartProduct[];
+
   return (
     <div className="bg-white py-10 px-20">
       <h1 className="font-bold text-2xl text-left">Giỏ hàng của bạn</h1>
       <hr className="h-1 bg-primary rounded-full my-4" />
-      <div className="flex items-start w-full">
-        <ItemsList className="flex-grow"></ItemsList>
-        <CheckoutInformation></CheckoutInformation>
-      </div>
+      {products.length === 0 && <NoValue>Không có sản phẩm nào trong giỏ</NoValue>}
+      {products.length !== 0 && (
+        <div className="flex items-start w-full gap-10">
+          <ItemsList products={products} className="flex-grow"></ItemsList>
+          <CheckoutInformation products={products} className="w-4/12"></CheckoutInformation>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/cart/ItemsList.tsx
+++ src/pages/cart/ItemsList.tsx
@@ -1,19 +1,18 @@
 import CartItem from '@components/Cart/CartItem/CartItem';
-import NoValue from '@components/Common/NoValue/NoValue';
 import ClassNameProps from '@model/ClassNameProps';
 import { CartProduct } from '@model/Product';
-import { RootState } from '@model/ReduxType';
 import React from 'react';
-import { useSelector } from 'react-redux';
 
-interface Props {}
+interface Props {
+  products: CartProduct[];
+}
 
 const ItemsList: React.FC<Props & ClassNameProps> = (props): JSX.Element => {
-  const products = useSelector<RootState>((state) => state.cart.products) as CartProduct[];
   return (
     <div className={props.className}>
-      {products.length === 0 && <NoValue>Không có sản phẩm nào trong giỏ</NoValue>}
-      {products.length !== 0 && products.map((product) => <CartItem key={product.id} item={product}></CartItem>)}
+      {props.products.map((product) => (
+        <CartItem key={product.id} item={product}></CartItem>
+      ))}
     </div>
   );
 };
